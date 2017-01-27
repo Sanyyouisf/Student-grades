008166510e7accd1393a250e4bce5da98d026c25
Sanyyouisf <sanymamdoh@yahoo.com>
2017-01-26 19:00:49 -0600

    for loop for the arrays

---
+++ grades.js
@@ -0,0 +1,57 @@
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+var grade =[];
+for (var i =0 ; i < scores.length ;i =i +1) {
+if (scores[i] >=50 && scores[i]<=60 ){
+grade.push ("F"); 
+console.log (grade);
+} else if (scores[i] >= 61 && scores[i]<= 70) {
+grade.push ("E"); 
+console.log (grade);
+} else if (scores[i] >=71 && scores[i]<=80) {
+grade.push ("C"); 
+console.log (grade);
+} else if (scores[i] >=81 && scores[i]<=90) {
+grade.push ("B"); 
+console.log (grade);
+} else if (scores[i] >=91 && scores[i]<=100) {
+grade.push ("A"); 
+console.log (grade);
+}
+}
+
+console.log(grade);
+document.write (grade);
+
+
+for (var j=0 ; j< grade.length ;j = j+1){
+if ( ){
+
+
+}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
